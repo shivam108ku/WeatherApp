e140949ade52071026980977d5c658be2eabf287
Shivam Kumar <shivam108ku@gmail.com>
2025-08-29 16:25:54 +0530

    addign class

---
+++ src/api/weather.ts
@@ -0,0 +1,20 @@
+class WeatherAPI{
+
+    private createUrl(endpoint:string,
+         params:Record<string,string | numbers>){
+
+    }
+    private fetchData(){
+        
+    }
+     async getCurrentWeather(){
+        
+    }
+     async getForecast(){
+        
+    }
+     async reverseGeocode(){
+        
+    }
+
+}

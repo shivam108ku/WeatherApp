229ba14634d7abab63daab684ad3fa25f4825c7f
Shivam Kumar <shivam108ku@gmail.com>
2025-08-29 15:57:56 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import Layout from "./components/layout"
 import { ThemeProvider } from "./context/theme-provider"
 import WeatherDashboard from "./pages/weather-dashboard"
 import CityPage from "./pages/city-page"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
+     <QueryClientProvider client={queryClient}>
      <BrowserRouter>
      <ThemeProvider defaultTheme="dark">
      <Layout>
@@ -17,6 +22,8 @@ const App = () => {
      </Layout>
      </ThemeProvider>
      </BrowserRouter>
+     <ReactQueryDevtools initialIsOpen={false} />
+     </QueryClientProvider>
   )
 }
 
